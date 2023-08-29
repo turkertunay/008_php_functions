74ff162a925be6c3a346697afc369422059cdc1f
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-08-29 14:08:36 +0300

    Add files via upload

---
+++ 008_functions.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+  <?php
+      function sayHi($name, $age){
+          echo "Hello $name, you are $age <br>";
+      }
+      sayHi("Sheldon", 15);
+      sayHi("Leonard", 20);
+      sayHi("Penny", 25);
+      sayHi("Howard", 30);
+      sayHi("Amy", 35);
+      sayHi("Rajesh", 40);
+
+  ?>
+
+
+  </body>
+</html>
